c086821bd08f8fc26c6923278663c087a0c533cf
helderhernandez <josue10hd@gmail.com>
2024-04-20 07:01:06 -0700

    Se agrego proyecot en U2 de prueba de Swift UI

---
+++ unidad-02/MyFirstTestSwift/MyFirstTestSwift/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  MyFirstTestSwift
+//
+//  Created by MacOsX on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            // VStack ****************
+            Text("*********** VStack ***********")
+            
+            VStack(alignment: .leading){
+                Text("Primer Text")
+                Text("Segundo Text")
+                Text("Tercer Text")
+            }
+            
+            Text("*********** VStack ***********")
+            VStack(alignment: .leading){
+                Rectangle()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                
+                Circle()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            
+            // HStack ****************
+            Text("*********** HStack ***********S")
+            
+            HStack(){
+                Text("Primer Text")
+                Text("Segundo Text")
+                Text("Tercer Text")
+            }
+            
+            Text("*********** HStack ***********S")
+            HStack(){
+                Rectangle()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.red)
+                
+                Circle()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unidad-02/MyFirstTestSwift/MyFirstTestSwift/MyFirstTestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstTestSwiftApp.swift
+//  MyFirstTestSwift
+//
+//  Created by MacOsX on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstTestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
