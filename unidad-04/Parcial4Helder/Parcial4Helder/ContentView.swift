5f29e78aa062577309639db895a5e0761c41e855
helderhernandez <macosx@iMac-de-MacOsX.local>
2024-05-18 12:58:02 -0700

    Se agrego proyecto de parcial 4

---
+++ unidad-04/Parcial4Helder/Parcial4Helder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parcial4Helder
+//
+//  Created by MacOsX on 5/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unidad-04/Parcial4Helder/Parcial4Helder/Parcial4HelderApp.swift
@@ -0,0 +1,29 @@
+//
+//  Parcial4HelderApp.swift
+//  Parcial4Helder
+//
+//  Created by MacOsX on 5/18/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct Parcial4HelderApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
